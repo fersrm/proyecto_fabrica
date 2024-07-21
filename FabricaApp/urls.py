73fb4d6c0d8891e9e4f99b2ca13b5f4c211ac056
Fernando Rosas <fer.rosasmardones@gmail.com>
2024-07-21 18:35:46 -0400

    tabla

--- FabricaApp/urls.py
+++ FabricaApp/urls.py
@@ -4,4 +4,6 @@ from FabricaApp import views
 urlpatterns = [
     path("", views.ProyectoInternoCreateView.as_view(), name="FabriCreate"),
     path("list/", views.ProyectoInternoListView.as_view(), name="FabriList"),
+    path('ficha/<int:id>/', views.ProyectoInternoDetailView.as_view(), name='FabriDetail'),
+    path("eliminar/<int:pk>/", views.ProyectoInternoDeleteView.as_view(), name="FabriDelete"),
 ]
--- FabricaApp/views.py
+++ FabricaApp/views.py
@@ -1,12 +1,13 @@
 from .models import FormularioProyectoInterno
 from .forms import ProyectoInternoCreateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView,DetailView,DeleteView
 from django.contrib import messages
 from django.urls import reverse_lazy
-from django.shortcuts import redirect
+from django.shortcuts import redirect,get_object_or_404
 from django.db.models import Q
 from django.core.paginator import Paginator
+from core.mixins import PermitsPositionMixin
 
 # Create your views here.
 
@@ -36,7 +37,7 @@ class ProyectoInternoCreateView(LoginRequiredMixin, CreateView):
 class ProyectoInternoListView(LoginRequiredMixin, ListView):
     model = FormularioProyectoInterno
     template_name = "pages/fabrica/fabrica_lista.html"
-    paginate_by = 1
+    paginate_by = 8
 
     def get_queryset(self):
         queryset = super().get_queryset().order_by("-id")
@@ -58,3 +59,23 @@ class ProyectoInternoListView(LoginRequiredMixin, ListView):
         context["object_list"] = paginator.get_page(page)
         context["placeholder"] = "Buscar por run, nombre, apellido o correo."
         return context
+
+
+class ProyectoInternoDetailView(LoginRequiredMixin, DetailView):
+    model = FormularioProyectoInterno
+    template_name = "pages/fabrica/fabrica_detalle.html"
+    context_object_name = "item"
+    
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(FormularioProyectoInterno, id=id_)
+    
+class ProyectoInternoDeleteView(LoginRequiredMixin, PermitsPositionMixin, DeleteView):
+    model = FormularioProyectoInterno
+    success_url = reverse_lazy("FabriList")
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        messages.success(self.request, "Proyecto eliminado correctamente")
+        self.object.delete()
+        return redirect(self.get_success_url())
